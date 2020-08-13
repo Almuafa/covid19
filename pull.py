482d21c1e7650b0307d1c4dcadb0b426182c4fb4
Almuafa <al_muafa@yahoo.com>
2020-08-12 22:36:58 -0500

    'COVID-19 pandemic in the United States - Wikipedia'

--- pull.py
+++ pull.py
@@ -1,2 +1,18 @@
 from bs4 import BeautifulSoup as bs
 import requests
+r = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
+suop = bs(r , 'lxml')
+title = suop.title.string
+results = suop.find_all('tr' , attrs={"class":"mw-collapsible"})
+records = []
+for result in results:
+    date = result.contents[1].text
+    confirmed = result.find('span' , class_="cbs-ibr").text
+    deaths = result.contents[-2].text
+    records.append((date, confirmed, deaths))
+
+records
+import pandas as pd 
+df = pd.DataFrame(records , columns=['date' ,'confirmed' ,'deaths'])
+df['date'] = pd.to_datetime(df['date'] , errors='coerce')
+df.to_csv("Desktop/COVID-19 pandemic in the USA - Wikipedia.csv" , index=False , encoding='utf-8')
