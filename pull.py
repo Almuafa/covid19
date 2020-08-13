b81d6116892f363082a7c3a25c5ab64814bc693b
Almuafa <al_muafa@yahoo.com>
2020-08-13 18:14:04 -0500

    COVID-19 pandemic in the United States by state and territory

--- pull.py
+++ pull.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd 
+
+# COVID-19 pandemic in the USA by date- Wikipedia
+
 r = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
 suop = bs(r , 'lxml')
 title = suop.title.string
@@ -12,7 +16,30 @@ for result in results:
     records.append((date, confirmed, deaths))
 
 records
-import pandas as pd 
+
+##### get a data to the pandas to get data frame
+
 df = pd.DataFrame(records , columns=['date' ,'confirmed' ,'deaths'])
 df['date'] = pd.to_datetime(df['date'] , errors='coerce')
 df.to_csv("Desktop/COVID-19 pandemic in the USA - Wikipedia.csv" , index=False , encoding='utf-8')
+
+# COVID-19 pandemic in the United States by state and territory
+
+link = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
+soup1=bs(link , "html.parser")
+table = soup1.find('table', attrs={"class" : "wikitable"})
+rows = table.select("tbody tr")[3:-3]
+
+data = []
+for row in rows:
+    state = row.find_all('th' , attrs = {'scope' : 'row'})[1].find('a').text
+    cases = row.find_all('td')[0].text.replace("\n", "")
+    deaths = row.find_all('td')[1].text.replace("\n","")
+    recoveries = row.find_all('td')[2].text.replace("\n","")
+    data.append((state, cases, deaths , recoveries))
+
+##### get a data to the pandas to get data frame
+
+df1 = pd.DataFrame(data , columns=['state', 'cases', 'deaths' , 'recoveries'])
+df1.to_csv("Desktop/COVID-19 pandemic in the USA by states.csv" , index=False )
+
