e203282d23f8adc3e28f5ef12ccf22ca5dd269ab
Almuafa <al_muafa@yahoo.com>
2020-08-18 10:31:32 -0500

    update

---
+++ covid.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+def pull_state_data_by_date(state):
+    """
+    name: pull_state_data_by_date
+    desc: this function pulls the data by state as argument and returns a dataframe
+    
+    input: state (name of the state, for ex., Texas)
+    output: dataframe (pandas dataframe)
+    
+    output: date, state, cases, death
+    
+    example invocation: 
+        dataframe = pull_state_data_by_date('Texas')
+    """
+    
+    # building url
+    link = requests.get(f'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_{state}').text
+    
+    # parsing the page
+    soup = bs(link , 'lxml')
+    results = soup.find_all('tr', attrs={"class":"mw-collapsible"})
+    
+    # extract date, confirmed, deaths from the record by looping thru them
+    records = []
+    for result in results:
+        date = result.contents[1].text
+        confirmed = result.find('span', class_="cbs-ibr").text
+        deaths = result.find_all('span', class_="cbs-ibr")[1].text
+        records.append((date, confirmed, deaths))
+    
+    # create dataframe
+    df = pd.DataFrame(records , columns=['date' ,'confirmed' ,'deaths'])
+    
+    # converting date to date format
+    df['date'] = pd.to_datetime(df['date'] , errors='coerce')
+    
+    # add state column
+    df['state'] = f'{state}'
+ 
+    return df
+
+def pull_state_data_by_county(state):
+    # not implemented
+    return None
--- pull.py
+++ pull.py
@@ -1,45 +1,102 @@
-from bs4 import BeautifulSoup as bs
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import requests
-import pandas as pd 
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-# COVID-19 pandemic in the USA by date- Wikipedia
 
-r = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
-suop = bs(r , 'lxml')
-title = suop.title.string
-results = suop.find_all('tr' , attrs={"class":"mw-collapsible"})
-records = []
-for result in results:
-    date = result.contents[1].text
-    confirmed = result.find('span' , class_="cbs-ibr").text
-    deaths = result.contents[-2].text
-    records.append((date, confirmed, deaths))
+# In[2]:
 
-records
 
-##### get a data to the pandas to get data frame
+# from covid import pull_recent_state_data # redudant
+from covid import pull_state_data_by_date
+from covid import pull_state_data_by_county
 
-df = pd.DataFrame(records , columns=['date' ,'confirmed' ,'deaths'])
-df['date'] = pd.to_datetime(df['date'] , errors='coerce')
-df.to_csv("Desktop/COVID-19 pandemic in the USA - Wikipedia.csv" , index=False , encoding='utf-8')
 
-# COVID-19 pandemic in the United States by state and territory
+# In[3]:
 
-link = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
-soup1=bs(link , "html.parser")
-table = soup1.find('table', attrs={"class" : "wikitable"})
-rows = table.select("tbody tr")[3:-3]
 
-data = []
-for row in rows:
-    state = row.find_all('th' , attrs = {'scope' : 'row'})[1].find('a').text
-    cases = row.find_all('td')[0].text.replace("\n", "")
-    deaths = row.find_all('td')[1].text.replace("\n","")
-    recoveries = row.find_all('td')[2].text.replace("\n","")
-    data.append((state, cases, deaths , recoveries))
+# unit testing
+# dataframe = pull_state_data_by_date('Alabama')
+# dataframe.head()
 
-##### get a data to the pandas to get data frame
 
-df1 = pd.DataFrame(data , columns=['state', 'cases', 'deaths' , 'recoveries'])
-df1.to_csv("Desktop/COVID-19 pandemic in the USA by states.csv" , index=False )
+# In[4]:
+
+
+# reading states
+states = open('states.lst', 'r').read().split(',')
+
+# pull the states data by looping through the list
+for state in states:
+    state = state.lstrip()
+    print(f'pulling data for state: {state}')
+    df = pull_state_data_by_date(state)
+    
+    print(f'writing to csv, for state: {state}')
+    df.to_csv(f'data\\{state}.csv', index=False)
+    print(f'file written to: data\\{state}.csv')
+    print('')
+
+# load the csv files into database
+# in the database, there will 2 tables
+# states (date, state, case, death)
+#   jan 1st 2020, TX, 123,123
+#   Jan 1st 2020, AL, 34, 234
+# counties (state, county, case, death, population)
+
+
+# query
+
+
+
+
+# from bs4 import BeautifulSoup as bs
+# import requests
+# import pandas as pd 
+
+# # COVID-19 pandemic in the USA by date- Wikipedia
+
+# r = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
+# suop = bs(r , 'lxml')
+# title = suop.title.string
+# results = suop.find_all('tr' , attrs={"class":"mw-collapsible"})
+# records = []
+# for result in results:
+#     date = result.contents[1].text
+#     confirmed = result.find('span' , class_="cbs-ibr").text
+#     deaths = result.contents[-2].text
+#     records.append((date, confirmed, deaths))
+
+# records
+
+# ##### get a data to the pandas to get data frame
+
+# df = pd.DataFrame(records , columns=['date' ,'confirmed' ,'deaths'])
+# df['date'] = pd.to_datetime(df['date'] , errors='coerce')
+# df.to_csv("Desktop/COVID-19 pandemic in the USA - Wikipedia.csv" , index=False , encoding='utf-8')
+
+# # COVID-19 pandemic in the United States by state and territory
+
+# link = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
+# soup1=bs(link , "html.parser")
+# table = soup1.find('table', attrs={"class" : "wikitable"})
+# rows = table.select("tbody tr")[3:-3]
+
+# data = []
+# for row in rows:
+#     state = row.find_all('th' , attrs = {'scope' : 'row'})[1].find('a').text
+#     cases = row.find_all('td')[0].text.replace("\n", "")
+#     deaths = row.find_all('td')[1].text.replace("\n","")
+#     recoveries = row.find_all('td')[2].text.replace("\n","")
+#     data.append((state, cases, deaths , recoveries))
+
+# ##### get a data to the pandas to get data frame
+
+# df1 = pd.DataFrame(data , columns=['state', 'cases', 'deaths' , 'recoveries'])
+# df1.to_csv("Desktop/COVID-19 pandemic in the USA by states.csv" , index=False )
 
