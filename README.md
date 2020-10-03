9631acd592d1e1f7b550104f664c7ea759f7b064
Almuafa <al_muafa@yahoo.com>
2020-10-03 13:22:48 -0500

    added few more scripts and finished editing README.md

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-# covid19
-This project is about covid19 data analysis in the USA
-I will Web Scraping using BeautifulSoup 
+# COVID19
+Web Scraping COVID19 data from public websites using BeautifulSoup
 
-then i will use pandas library to save the data as csv
+## Workflow
+Description of python scripts and python modules used as part of this workflow
+
+Below are the scripts used
+- `covid.py`
+- `pull.py`
+### covid.py
+This script consists of modules that will called by the [`pull.py`](pull.py) script, such as
+- pull_states
+- pull_state_data_by_date
+- pull_state_data_by_county
+
+#### pull_states
+This function pulls all the states which will be used by other modules
+
+#### pull_state_data_by_date
+This function pulls the data by state as argument and returns a dataframe
+
+#### pull_state_data_by_county
+This function pulls the data by county as argument and returns a dataframe
+
+### pull.py
+This script is the main orchestrator which uses [`covid.py`](covid.py) and its modules to pull the data from the public websites.
+
+## Usage
+Please run the script using the following command
+
+```python
+python pull.py
+```
+*alternatively you can run the jupyter notebook `covid19.ipynb` included as part of the repo*
+
+## References
+This repo used the following references in coming up with all code etc.,
+- https://docs.python.org/3/library/functions.html#open
+
+## Feedback
+Please create GitHub issues, if you find any issues or bugs or ideas to improve upon
--- covid.py
+++ covid.py
@@ -2,6 +2,27 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
+def pull_states():
+    """
+    name: pull_states
+    desc: this function pulls all the states which will be used by other modules
+    
+    input: None
+    output: list (list of all states)
+    
+    example invocation: 
+        states = pull_states()
+    """
+    link = requests.get('https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States').text
+    soup = bs(link , "html.parser")
+    table = soup.find('table', attrs={'class': 'wikitable', 'class': 'sortable'})
+    rows = table.select("tbody tr")[3:-3]
+    states = []
+    for row in rows:
+        state = row.find_all('th' , attrs = {'scope' : 'row'})[1].find('a').text.replace(" ", "_")
+        states.append(state)
+    return states
+    
 def pull_state_data_by_date(state):
     """
     name: pull_state_data_by_date
@@ -9,8 +30,6 @@ def pull_state_data_by_date(state):
     
     input: state (name of the state, for ex., Texas)
     output: dataframe (pandas dataframe)
-    
-    output: date, state, cases, death
     
     example invocation: 
         dataframe = pull_state_data_by_date('Texas')
@@ -37,16 +56,15 @@ def pull_state_data_by_date(state):
     # converting date to date format
     df['date'] = pd.to_datetime(df['date'] , errors='coerce')
     
-    
     # add state column
     df['state'] = f'{state}'
  
-    return df
+    return df 
 
 def pull_state_data_by_county(state):
     """
     name: pull_state_data_by_county
-    desc: this function pulls the data by state as argument and returns a dataframe
+    desc: this function pulls the data by county as argument and returns a dataframe
     
     input: state (name of the state, for ex., Texas)
     output: dataframe (pandas dataframe)
@@ -54,16 +72,14 @@ def pull_state_data_by_county(state):
     example invocation: 
         dataframe = pull_state_data_by_county('Texas')
     """
-    
     # building url
     url = f'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_{state}'
     r = requests.get(url)
-    
     # parsing the page and the data
     soup = bs(r.text, 'html.parser')
     table = soup.find('table', {'class': 'wikitable', 'class': 'sortable'})
     
     # read the table and create dataframe
     df_county = pd.read_html(str(table))
-       
-    return df_county[0]  
+        
+    return df_county[0]
